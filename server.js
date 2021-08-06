fa6473443c9ec5f628118f73d73341ca3aefee5d
Basmaelsisy <72557614+Basmaelsisy@users.noreply.github.com>
2021-08-06 21:16:58 +0200

    Add files via upload

---
+++ server.js
@@ -0,0 +1,57 @@
+// Setup empty JS object to act as endpoint for all routes
+projectData = {};
+
+// Require Express to run server and routes
+const express= require('express');
+
+// Start up an instance of app
+const app = express();
+
+/* Middleware*/
+const bodyParser = require ('body-parser') 
+
+//Here we are configuring express to use body-parser as middle-ware.
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Cors for cross origin allowance
+const cors =require ('cors');
+app.use (cors()); 
+
+// Initialize the main project folder
+app.use(express.static('website'));
+
+
+// Setup Server
+const port =8000;
+const server =app.listen(
+    port , listining
+)
+
+function listining(){
+    console.log(  `server Running On Localhost: ${port}`)
+}
+
+// create a callBack function and send the data to 'all' file 
+//get route
+app.get('/all' ,function(req,res){
+res.send(projectData);
+console.log(projectData);
+})
+
+
+
+// post route 
+
+app.post('/add' , function(req,res){
+    let data = req.body;
+
+    console.log('POST Update to server ', data);
+
+    projectData['temp'] = data.temp;
+    projectData['date'] = data.date;
+    projectData['feelings'] = data.feelings;
+
+    //send project data
+    res.send(projectData);
+});
---
+++ website/app.js
@@ -0,0 +1,72 @@
+// Global Variables 
+const baseUrl = 'https://api.openweathermap.org/data/2.5/weather?q=';
+const apiKey = 'AIzaSyBXswvptD09rR_FhfAv74u1iNzF86-qf80';
+
+// Create a new date instance dynamically with JS
+let date = new Date();
+let newDate = date.getMonth()+1+'.'+ date.getDate()+'.'+ date.getFullYear();
+console.log('The date is' + newDate);
+
+// Event listener to add function to existing HTML DOM element
+
+document.getElementById('generate').addEventListener('click' , generateWheather);
+
+function  generateWheather (e){
+    const zip = document.getElementById('zip').value;
+    const feeling = document.getElementById('feelings').value;
+
+    retrieveWeather(baseUrl , apiKey , zip)
+
+   .then(function(data){
+        console.log(data);
+      // Add all data into POST request
+          //The call to method `postData` should have endpoint name as `/add` -- as per mentor instruction https://knowledge.udacity.com/questions/629283
+          postData('/add', {temp:data.main.temp, date:newDate, feelings:feeling});
+          //run userView function
+      })
+    .then( () => userView());
+      }
+
+// GET web ApI function 
+const retrieveWeather = async(baseUrl , zip , apiKey) => {
+    const res = await fetch(baseUrl+zip+apiKey);
+    try{
+        const data = await res.json();
+        console.log(data);
+        return data;
+    } 
+    catch(error){
+        console.log("error" , error);
+    }
+}
+
+// postData function 
+const postData = async ( apiUrl = '' , data={})=>{
+    console.log(data);
+    const response = await fetch(apiUrl ,{
+        method : 'post',
+        credentials: 'same-origin',
+        headers:{
+            'content-type':'application/json'
+        },
+        body:JSON.stringify(data)
+    })
+    try {
+        const newData =await response.json();
+        console.log(newData);
+        return newData;
+    }catch(error){
+        console.log('error' ,error)
+    }
+}
+const userView= async()=>{
+    const request =await fetch('/allWeaterData');
+    try{
+        const allData= await request.json();
+        document.getElementById('date').innerHTML=`date: ${allData.date}`;
+        document.getElementById('temp').innerHTML=`the temperature is : ${allData.temp}`;
+        document.getElementById('content').innerHTML=`my feeling is : ${allData.content}`;
+    }catch(error){
+        console.log('error',error);
+    }
+}
